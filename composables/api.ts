b4a71d1f2bee8af4ba31fdc2cffd525ac179d1fd
rafik kasmi <rafik@dts-solution.com>
2024-07-06 00:07:18 +0100

    [fix]

--- composables/api.ts
+++ composables/api.ts
@@ -1,7 +1,7 @@
 // Create a fetch object with a baseURL already setup
 const API_KEY = "fc0f0776a79e75e8888a8335a281da5c"
 
-export const movieAPI = (url, page = 1, options = {}) => useFetch(`https://api.themoviedb.org/3/${url}?api_key=${API_KEY}&page=${page}`, options)
+export const movieAPI = (url, page = 1, options = {}) => useFetch(`https://api.themoviedb.org/3${url}?api_key=${API_KEY}&page=${page}`, options)
 
 export const searchMovie = (query, page = 1, options = {}) => useFetch(`https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&query=${query}&page=${page}`, options)
 
